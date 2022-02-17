9d11c5528407fd0000247d9eb3ba611958e3cd7f
unknown <lipsius.m@gmail.com>
2022-02-17 13:14:39 -0700

    final commit

--- Assign1/Sorters/AllSorts.java
+++ Assign1/Sorters/AllSorts.java
@@ -5,8 +5,8 @@ import Shapes.SubShapes.*;
 import Shapes.SubPrisms.*;
 import java.util.*;
 import java.io.*;
-public class AllSorts {
 
+public class AllSorts {
 
     public static Shape [] bubbleSort(Shape [] inputArray){
         boolean sorted = false;
@@ -19,7 +19,7 @@ public class AllSorts {
 
             for (int i = 0; i < alteredArray.length - 1; i++) {
 
-                if (alteredArray[i].getHeight() >  alteredArray[i+1].getHeight()){
+                if (alteredArray[i].getHeight() > alteredArray[i+1].getHeight()){
                     tempShape = alteredArray[i];
                     alteredArray[i] = alteredArray[i+1];
                     alteredArray[i+1] = tempShape;
@@ -30,23 +30,23 @@ public class AllSorts {
         return alteredArray;
     }
 
-    public static void selectionSort(Shape [] inputArray){
+    // public static Shape [] selectionSort(Shape [] inputArray){
 
-    }
+    // }
 
-    public static void insertionSort(Shape [] inputArray){
+    // public static Shape [] insertionSort(Shape [] inputArray){
 
-    }
+    // }
 
-    public static void mergeSort(Shape [] inputArray){
+    // public static Shape [] mergeSort(Shape [] inputArray){
 
-    }
+    // }
 
-    public static void quickSort(Shape [] inputArray){
+    // public static Shape [] quickSort(Shape [] inputArray){
 
-    }
+    // }
 
-    public static void radixSort(Shape [] inputArray){
+    // public static Shape [] radixSort(Shape [] inputArray){
 
-    }
+    // }
 }
